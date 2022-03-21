89955efbbe935fbfcd7675b8188dd3e68ff6b114
Yeonho Jang <siabard@gmail.com>
2022-03-21 15:52:25 +0900

    신규 맵지도 추가

---
+++ resources/cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="cave" tilewidth="16" tileheight="16" tilecount="80" columns="16">
+ <image source="Stage/PrtCave.pbm" width="256" height="80"/>
+</tileset>
